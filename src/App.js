b4684197733ee00ded89a1afc6036ee888b925e2
toni249 <imtony1304@gmail.com>
2023-12-06 12:31:38 +0530

    backend is ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [conversation, setConversation] = useState([]);
+
+  const sendMessage = async () => {
+    const newConversation = [...conversation, { user: 'user', text: input }];
+    // console.log(newConversation);
+    setConversation(newConversation);
+    setInput('');
+
+    try {
+      const response = await axios.post('http://localhost:5000/ask', { prompt: input });
+      const botResponse = response.data.response;
+      setConversation([...newConversation, { user: 'bot', text: botResponse }]);
+    } catch (error) {
+      console.error('Request to backend failed:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {conversation.map((message, index) => (
+          <div key={index}>
+            <span>{message.user}: </span>
+            <span>{message.text}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
